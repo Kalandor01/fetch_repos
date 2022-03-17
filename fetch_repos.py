23113cbe5f07d67fcc6c0d71876aa8fac8777d10
Kalandor01 <rohovszkyakoska@gmail.com>
2022-03-17 14:02:03 +0100

    file rename

--- fetch_repos.py
+++ fetch_repos.py
@@ -1,29 +1,4 @@
-import os
 import requests
-
-
-# local projects
-def local_get():
-    # html
-    f = open("links/html.txt", "w")
-    root = "./links/web"
-    for name in os.listdir(root):
-        if not os.path.isfile(os.path.join(root, name)):
-            f.write(name + "\n")
-    f.close()
-
-    # other
-    others = ["py", "java"]
-    for other in others:
-        f = open(f"links/{other}.txt", "w")
-        for _, _, files in os.walk(f"links\\file\{other}"):
-            for name in files:
-                try:
-                    f.write(name + "\n")
-                except UnicodeEncodeError:
-                    print("\r[UnicodeEncodeError]")
-        f.close()
-    print("Local HTML, Python and Java project list updated.\n")
 
 
 def conflicts(added_lis, removed_lis):
